e2ee4eb5e1bcb7515612ce6bf4799215b2fb6b8d
Lars Dalby <lars.dalby@gmail.com>
2018-04-08 19:05:39 +0200

    Add content to tidyverse example

--- r/02_tidyverse-example.R
+++ r/02_tidyverse-example.R
@@ -2,3 +2,26 @@
 # Author Lars Dalby
 
 library(tidyverse)
+
+# The iris data as data.frame
+iris
+
+# Same as tibble
+iris %>% 
+  as_tibble()
+
+# Select Species and Petal.xxx columns
+iris %>% 
+  as_tibble() %>% 
+  select(Species,
+         starts_with("Petal"))
+
+# Filter to only Sepal.Width <= 3
+iris %>% 
+  as_tibble() %>% 
+  filter(Sepal.Width <= 3)
+
+# Calculate new area column
+iris %>% 
+  as_tibble() %>% 
+  mutate(Sepal.Area = Sepal.Length * Sepal.Width)
