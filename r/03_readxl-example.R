4f46b504127fe5a0936a38c7388b4f94dfcecee2
Lars Dalby <lars.dalby@gmail.com>
2018-04-09 09:20:47 +0200

    Fix #1

---
+++ r/03_readxl-example.R
@@ -0,0 +1,20 @@
+# IRSAE R workshop, Kaloe
+# Date April 9th 2018
+# Author Lars Dalby
+
+library(readxl)
+library(here)
+
+# Let's read the data:
+read_excel(here("data", "readxl-example-data.xlsx"))
+
+# Hmm wrong sheet, let's try again
+read_excel(here("data", "readxl-example-data.xlsx"),
+           sheet = "data")
+
+# Better, but quite messy. Let's see if we can get only the data
+read_excel(here("data", "readxl-example-data.xlsx"),
+           sheet = "data", range = cell_cols("C:E"))
+
+# There are plenty more options, for when things get really ugly
+?read_excel
