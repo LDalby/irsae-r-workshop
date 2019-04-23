cf42a7ca165b492f8c8e45fd87639a73dc9e005a
LDalby <lars.dalby@gmail.com>
2019-04-23 09:41:01 +0200

    Add purrr example

---
+++ r/04_purrr-example.R
@@ -0,0 +1,17 @@
+# IRSAE R workshop, Kaloe
+# Date April 24th 2019
+# Author Lars Dalby
+# This example is adapted from:
+# https://serialmentor.com/blog/2016/6/13/reading-and-combining-many-tidy-data-files-in-R
+
+fs::dir_ls(here("data")) %>% 
+  str_subset("soil_data") -> files
+
+tibble(file_path = files) %>% 
+  mutate(file_contents = map(file_path, ~ read_tsv(., col_types = "di")),
+         file_name = fs::path_file(file_path))  
+
+data %>% 
+  select(-file_path) %>% 
+  unnest() %>% 
+  mutate(date = ymd(str_sub(file_name, end = 10)))
