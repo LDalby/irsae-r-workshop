ebaf8bff82aeec131358f43eca09dc9de68efd9b
LDalby <lars.dalby@gmail.com>
2019-04-24 10:47:08 +0200

    Update forest data

--- r/helper_02_forest-exercise.R
+++ r/helper_02_forest-exercise.R
@@ -7,9 +7,7 @@ library(readxl)
 library(pathfinder)
 
 read_sf("/Users/au206907/Downloads/lada/plot_proj.shp",
-        as_tibble = TRUE) %>% 
-  rename(pole_id = POLE_ID,
-         plot_nr = PLOTNR_) -> plots
+        as_tibble = TRUE) -> plots
 
 write_sf(plots, dsn = here("data", "forest", "forest.gpkg"), layer = "plots")
 
@@ -22,15 +20,5 @@ veg_data %>%
   mutate(foo = walk2(year, data, ~ write_tsv(.y, path = here("data",glue("{.x}_vegetation-data-kalo.tsv")))))
 
 
-fs::dir_ls(here("data", "forest")) %>% 
-  str_subset("vegetation-data") -> files
 
-tibble(file_path = files) %>% 
-  mutate(file_contents = map(file_path, ~ read_tsv(., col_types = "icccdc")),
-         file_name = fs::path_file(file_path)) -> data
-
-data %>% 
-  select(-file_path) %>% 
-  unnest() %>% 
-  mutate(year = str_sub(file_name, end = 4))
 
